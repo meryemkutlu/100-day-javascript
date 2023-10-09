b726a571315589947c3e240665066db1855265ee
meryemkutlu <meryem.kutlu13@gmail.com>
2023-10-09 22:38:38 +0300

    finish project

--- day-26-page-scroll-indictor/app.js
+++ day-26-page-scroll-indictor/app.js
@@ -10,45 +10,29 @@ function smoothScroll(e) {
     document.querySelector(href).scrollIntoView({
         behavior: "smooth",
     });
-    hideMenu();
 }
 
-// RESPONSIVE MOBILE MENU
-const menuWrapper = document.querySelector(".nav-wrapper");
-const menu = document.querySelector(".nav-list");
-const hamburger = document.querySelector(".hamburger");
-const close = document.querySelector(".close");
-
-const showMenu = () => {
-    hamburger.style.display = "none";
-    close.style.transform = "translateY(0)";
-    menuWrapper.style.transform = "translateX(0)";
-    menu.style.transform = "translateX(0)";
-};
-
-const hideMenu = () => {
-    close.style.transform = "translateY(-20rem)";
-    hamburger.style.display = "block";
-    menuWrapper.style.transform = "translateX(-200%)";
-    menu.style.transform = "translateX(200%)";
-    subMenuThree.style.transform = "translateX(-100%)";
-};
-
-hamburger.addEventListener("click", showMenu);
-close.addEventListener("click", hideMenu);
-menuWrapper.addEventListener("click", hideMenu);
-
-// Submenu Section
-const thirdLink = document.querySelector(".third-link");
-const back = document.querySelector(".back-to-menu");
-const subMenuThree = document.querySelector(".submenu-three");
-
-thirdLink.addEventListener("click", () => {
-    menu.style.transform = "translateX(-100%)";
-    subMenuThree.style.transform = "translateX(0)";
+// Sticky Header
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-back.addEventListener("click", () => {
-    menu.style.transform = "translateX(0)";
-    subMenuThree.style.transform = "translateX(-100%)";
-});
+// Scroll Indicator JS
+window.onscroll = () => scrollProgress();
+
+function scrollProgress() {
+    const currentState =
+        document.body.scrollTop || document.documentElement.scrollTop;
+
+    const pageHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const scrollPercentage = (currentState / pageHeight) * 100;
+
+    const progressBar = document.querySelector(".progress");
+
+    progressBar.style.visibility = "visible";
+    progressBar.style.width = scrollPercentage + "%";
+}
