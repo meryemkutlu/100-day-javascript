fdcadb665cdfd4d99fde6ed7a0c57e5222d6e925
meryemkutlu <meryem.kutlu13@gmail.com>
2023-10-19 02:30:58 +0300

    add event listener

--- day-32-notes-app/app.js
+++ day-32-notes-app/app.js
@@ -11,3 +11,21 @@ function getNotes() {
         notesObj = JSON.parse(notes);
     }
 }
+//Note btn event listener
+noteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (noteTitle.value == "" || noteText.value == "") {
+        return alert("plaase add note title and details");
+    }
+
+    getNotes() // notesObj array
+
+    let myObj = {
+        title: noteTitle.value,
+        text: noteText.value,
+    }
+    notesObj.push(myObj)
+    localStorage.getItem("notes", JSON.stringify(notesObj));
+
+    document.querySelector("form").reset();
+});
